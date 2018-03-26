f2da64bec1df8c560bcba6af2273bcd645230280
zhangxiaokang <kangeneral@126.com>
2018-03-27 00:12:59 +0800

    Initial commit

---
+++ mooc-web/src/main/java/com/zhang/mooc/MoocApplication.java
@@ -0,0 +1,12 @@
+package com.zhang.mooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoocApplication.class, args);
+    }
+}
